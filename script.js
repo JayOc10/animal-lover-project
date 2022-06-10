ea4512fdd26e2173d1a5c4b85e48fced9126dc63
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-06-10 10:39:32 -0700

    js file

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    var currentDate = $("#currentDate");
+    const fName = JSON.parse(localStorage.getItem('firstName')) || [];
+
+    //This function updates the current date to 
+    //display at the top of the page
+    function updateCurrentDate() {
+        currentDate.text(moment().format("dddd, MMMM Do YYYY"));
+    }
+    
+    //function calls
+    updateCurrentDate();
+    // form.js
+    var tableBody = document.getElementById('repo-table');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = 'https://www.petrescue.com.au/api/listings';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      //Loop over the data to generate a table, each table row will have a link to the repo url
+      for (var i = 0; i < data.length; i++) {
+        // Creating elements, tablerow, tabledata, and anchor
+        var createTableRow = document.createElement('tr');
+        var tableData = document.createElement('td');
+        var link = document.createElement('a');
+
+        // Setting the text of link and the href of the link
+        link.textContent = data[i].html_url;
+        link.href = data[i].html_url;
+
+        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+        // The tablerow then gets appended to the tablebody
+        tableData.appendChild(link);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+      }
+    });
+}
+
+fetchButton.addEventListener('click', getApi);
+});
