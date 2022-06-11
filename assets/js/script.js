75734ec27b6de42e648dfb89ada5eae8ab309732
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-06-11 14:22:10 -0700

    added modals and favorites function to parse and stringify user's selected favorite breeds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var currentDate = $("#currentDate");
     var breed = "";
 
@@ -7,115 +7,124 @@ $(document).ready(function() {
     function updateCurrentDate() {
         currentDate.text(moment().format("dddd, MMMM Do YYYY"));
     }
-    
- 
+
+
     // form.js
     var tableBody = document.getElementById('repo-table');
     var fetchButton = document.getElementById('fetch-button');
-    
-function getApi() {
-  // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://api.thedogapi.com/v1/breeds?limit=20&page=0';
+    var modalTitle = document.getElementById('modal-title');
+    var modelFavBtn = document.getElementById("savedFavorites");
 
-  fetch(requestUrl, {
-    headers: {authentication: "x-api-key ea90132b-c88d-4529-afad-51de89d38cb9"}
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      for (var i = 0; i < data.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        tableData.className = "breed-name";
-        
-
-        // Setting the text of link and the href of the link
-        tableData.textContent = data[i].name;
-
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        // tableData.appendChild(link);
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
-    });
-    }
-      function getKey() {
+    function getApi() {
         // fetch request gets a list of all the repos for the node.js organization
-        
+        var requestUrl = 'https://api.thedogapi.com/v1/breeds?limit=20&page=0';
+
+        fetch(requestUrl, {
+            headers: { authentication: "x-api-key ea90132b-c88d-4529-afad-51de89d38cb9" }
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+                //Loop over the data to generate a table, each table row will have a link to the repo url
+                for (var i = 0; i < data.length; i++) {
+                    // Creating elements, tablerow, tabledata, and anchor
+                    var createTableRow = document.createElement('tr');
+                    var tableData = document.createElement('td');
+                    tableData.className = "breed-name";
+
+
+                    // Setting the text of link and the href of the link
+                    tableData.textContent = data[i].name;
+
+                    // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                    // The tablerow then gets appended to the tablebody
+                    // tableData.appendChild(link);
+                    tableData.addEventListener("click", getBreed);
+                    createTableRow.appendChild(tableData);
+                    tableBody.appendChild(createTableRow);
+                }
+
+            });
+    }
+    function getKey() {
+        // fetch request gets a list of all the repos for the node.js organization
+
         var token = ''
-      
-      fetch('https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        }, 
-        body:JSON.stringify({
-          "grant_type":"client_credentials","client_id":"qIVT27e1bB7qYrnovBGDoJdA5D2LFOfd2qpQEFc0Kq15i40qFt","client_secret":"mcP2VgYf89I6msMlkmkjdEadDwJAWbwjpvfjH4OC"
-      })
-      })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        token = data.access_token
-        getPetBreeds(token)
-      })
+
+        fetch('https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "grant_type": "client_credentials", "client_id": "qIVT27e1bB7qYrnovBGDoJdA5D2LFOfd2qpQEFc0Kq15i40qFt", "client_secret": "mcP2VgYf89I6msMlkmkjdEadDwJAWbwjpvfjH4OC"
+            })
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+                token = data.access_token
+                getPetBreeds(token)
+            })
     }
 
 
-function getPetBreeds(token) {
-  
-  var requestUrl = `https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/animals?breed=${breed}` ;
-  console.log(requestUrl);
-  fetch(requestUrl, {
-    headers: {"Authorization": `Bearer ${token}`}
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      //modal population
-      //button to add to favorites
-});
-}
-   //function calls
+    function getPetBreeds(token) {
+
+        var requestUrl = `https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/animals?breed=${breed}`;
+        console.log(requestUrl);
+        fetch(requestUrl, {
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+                //modal population
+                //button to add to favorites
+            });
+    }
+    //function calls
     updateCurrentDate();
     fetchButton.addEventListener('click', getApi);
-    tableBody.addEventListener("click", getBreed);
-  
+    
     function getBreed(event) {
         console.log(event.target);
         var clickedBreed = event.target;
-        if (clickedBreed.matches(".breed-name")) {
-            breed = clickedBreed.textContent;
-            console.log(breed);
-        }
-        getKey();
+        document.getElementsByClassName("modal-title")[0].textContent = clickedBreed.textContent;
+        document.getElementById("savedFavorites").setAttribute("animal", clickedBreed.innerText);
+        //getKey();
 
-        
+
+    }
+   
+
+    const favorites = (event) => {
+        var dogs = localStorage.getItem("dogs");
+        var dogsArray = JSON.parse(dogs||"[]");
+        dogsArray.push(event.target.getAttribute("animal"));
+        localStorage.setItem("dogs", JSON.stringify(dogsArray));
     }
 
-    //function addToFavorites
-    //local storage
-    //add to favorites
+    modelFavBtn.addEventListener("click", favorites);
+
+    //JSON.stringify turns anything in js into a string
+    //JSON.parse turns any string into js
 
     //function to grab from local storage
     //TODO:
-        //create modal
-        //create function to add to favorites and also
-        //set local storage
-        //create function to get local storage and display
-        //create new .js file to link to favorites.html
-        //JSON stingify and parse to store favs into array
+    //set up favorites page
+    //need to display dog info on modal from 2nd API
+    //create function to get local storage and display
 
-
-fetchButton.addEventListener('click', getApi);
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
+    fetchButton.addEventListener('click', getApi);
 
 });
